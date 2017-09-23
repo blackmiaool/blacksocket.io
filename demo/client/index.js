e698ae734ac1444cb760839b4e5ff092154ec876
blackmiaool <blackmiaool@gmail.com>
2017-09-23 18:54:20 +0800

    update demo

--- demo/client/index.js
+++ demo/client/index.js
@@ -1,4 +1,4 @@
-const io=require("client");
+const io=require("blacksocket.io/client");
 const socket = io(`:23033/test`);
 setInterval(()=>{
     socket.emit('client-event',{a:'client params'},function(cbParams){
